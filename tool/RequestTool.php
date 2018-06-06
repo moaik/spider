b1dd5e14a65df3e48bb8b1480374fdca7765c92b
moext <niucunhui@gmail.com>
2018-06-06 10:19:51 +0800

    add http post request

--- tool/RequestTool.php
+++ tool/RequestTool.php
@@ -16,6 +16,8 @@ class RequestTool
         $ch = curl_init();
         // set url
         curl_setopt($ch, CURLOPT_URL, $url);
+        // set request method
+        curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'GET');
         // set User-Agent
         curl_setopt($ch, CURLOPT_USERAGENT, 'qMotor/5.2.12 (iPhone; iOS 11.3.1; Scale/2.00)');
         // set Header
@@ -32,13 +34,35 @@ class RequestTool
         // close curl resource to free up system resources
         curl_close($ch);
         $ch = NULL;
-        
+
         return $result;
     }
 
-    public function post()
+    public function post($url)
     {
+        $ch = curl_init();
+        // set url
+        curl_setopt($ch, CURLOPT_URL, $url);
+        // set request method
+        curl_setopt($ch, CURLOPT_POST, true);
+        // set User-Agent
+        curl_setopt($ch, CURLOPT_USERAGENT, 'qMotor/5.2.12 (iPhone; iOS 11.3.1; Scale/2.00)');
+        // set Header
+        curl_setopt($ch, CURLOPT_HTTPHEADER, self::$headers);
+        // curl_exec 执行的结果不自动打印出来
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        // execute
+        $result = curl_exec($ch);
 
+        if (false === $result) {
+            return curl_error($ch) . curl_errno($ch);
+        }
+
+        // close curl resource to free up system resources
+        curl_close($ch);
+        $ch = NULL;
+
+        return $result;
     }
 
     public function setHeader($headers)
