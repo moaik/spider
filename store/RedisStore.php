e8290dde80c87757a1dc7f0d22f876b96671d758
moext <niucunhui@gmail.com>
2018-06-26 14:51:12 +0800

    proxy ip pool

--- store/RedisStore.php
+++ store/RedisStore.php
@@ -2,5 +2,27 @@
 
 class RedisStore
 {
+    private $redis;
 
+    public function __construct()
+    {
+        $this->redis = new Redis();
+        $this->redis->connect('127.0.0.1', 6379);
+    }
+
+    public function leftList($key, $value)
+    {
+        if (is_array($value)) {
+            foreach ($value as $val) {
+                $this->redis->lPush($key, $val);
+            }
+        } else {
+            $this->redis->lPush($key, $value);
+        }
+    }
+
+    public function getHead($key)
+    {
+        return $this->redis->lPop($key);
+    }
 }
